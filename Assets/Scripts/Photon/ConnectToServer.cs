b6748ef205fe50a06a2428b5406f0523e317aaf2
Facundo Cancela <105611511+FacundoCancela@users.noreply.github.com>
2024-08-22 19:51:03 -0300

    Photon Instalation

---
+++ Assets/Scripts/Photon/ConnectToServer.cs
@@ -0,0 +1,16 @@
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    private void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
